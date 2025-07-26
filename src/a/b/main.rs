3eba0b840f2ca500d4848edf38f57d0dd42f7fa1
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2025-07-25 21:11:42 -0400

    comments

--- src/a/b/main.rs
+++ src/a/b/main.rs
@@ -4,7 +4,9 @@ pub struct B {
 }
 // the import below is only there to demonstrate how importing from a module in
 // another directory works. It is not used.
-// The first "super" refers to directory "b" while the second "super" refers to
-// directory "a".
+// The first "super" refers to module "b" that is described in the file "../b.rs". This
+// "super" can be thought of as referring to directory "../b".
+// The second "super" refers to module "a" that is described in the file "../../a.rs".
+// This second "super" can be thought of as referring to directory "../../a"
 #[allow(unused_imports)]
 use super::super::c::main::C;
--- src/a/c/main.rs
+++ src/a/c/main.rs
@@ -4,7 +4,9 @@ pub struct C {
 }
 // the import below is only there to demonstrate how importing from a module in
 // another directory works. It is not used.
-// The first "super" refers to directory "c" while the second "super" refers to
-// directory "a".
+// The first "super" refers to module "c" that is described in the file "../c.rs". This
+// "super" can be thought of as referring to directory "../c".
+// The second "super" refers to module "a" that is described in the file "../../a.rs".
+// This second "super" can be thought of as referring to directory "../../a"
 #[allow(unused_imports)]
 use super::super::b::main::B;
