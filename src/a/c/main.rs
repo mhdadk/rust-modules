d5e68d0fda3d8bb8e48d3c76ee0f9677c631b3b4
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2025-07-25 21:04:27 -0400

    comments

--- src/a/c/main.rs
+++ src/a/c/main.rs
@@ -2,5 +2,7 @@ pub struct C {
     pub field1: i32,
     pub field2: i32,
 }
+// the import below is only there to demonstrate how importing from a module in
+// another directory works. It is not used.
 #[allow(unused_imports)]
 use super::super::b::main::B;
